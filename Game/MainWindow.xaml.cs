b0bbecd1e10da0e9d0984156f9f65f0358fc154b
Rakdav <rakdav@inbox.ru>
2023-03-03 15:04:40 +0200

    Добавьте файлы проекта.

---
+++ Game/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer=new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound=0;
+        TextBlock lastTextBlockClicked;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            SetUpGame();
+        }
+        private void Timer_Tick(Object sender,EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text=(tenthsOfSecondsElapsed/10F).ToString();
+            if (matchesFound == 0)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + ". Играть заново";
+            }
+        }
+        private void SetUpGame()
+        {
+            List<string> animalEmogi = new List<string>()
+            {
+                "🐶","🐱","🐭","🐹",
+                "🐰","🐻","🐼","🐨",
+                "🐯","🐷","🐸","🐵",
+                "🐔","🐦","🐤","🐉"
+            };
+            Random random=new Random();
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                if(textBlock.Name !="timeTextBlock")
+                {
+                    textBlock.Visibility=Visibility.Visible;
+                    int index=random.Next(animalEmogi.Count);
+                    string nextEmoji=animalEmogi[index];
+                    textBlock.Text=nextEmoji;
+                    animalEmogi.RemoveAt(index);  
+                }
+            }
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void timeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
